47ff67ed5855285cf4c1c1ebec920c2988a162e4
carlossilva23 <carlossilva23@arizona.edu>
2025-06-26 11:46:49 -0700

    Added Stub Files

---
+++ src/bs_pricer.py
@@ -0,0 +1,7 @@
+def black_scholes_call(S, K, r, T, sigma):
+    """Stub for Black-Scholes European call pricing."""
+    pass
+
+def black_scholes_put(S, K, r, T, sigma):
+    """Stub for Black-Scholes European put pricing."""
+    pass
---
+++ src/mc_pricer.py
@@ -0,0 +1,3 @@
+def monte_carlo_call_price(S0, K, r, T, sigma, num_sims):
+    """Stub for Monte Carlo European call option pricing."""
+    pass
